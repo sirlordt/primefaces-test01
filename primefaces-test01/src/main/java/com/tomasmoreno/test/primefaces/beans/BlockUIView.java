cf963560d198af488c665162906da38b9ede7aa0
Tomás Rafael Moreno Poggio <tomasmoreno@DSistemas01>
2016-07-23 09:12:30 -0400

    Initial release

---
+++ primefaces-test01/src/main/java/com/tomasmoreno/test/primefaces/beans/BlockUIView.java
@@ -0,0 +1,63 @@
+package com.tomasmoreno.test.primefaces.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import com.tomasmoreno.test.primefaces.data.Car;
+ 
+@ManagedBean
+public class BlockUIView {
+     
+    private String firstname;
+    private String lastname;
+    private List<Car> cars;
+     
+    @PostConstruct
+    public void init() {
+        cars = new ArrayList<Car>();
+        cars.add(new Car("Y25YIH5", "Fiat", 2014, "Black", 10000, true));
+        cars.add(new Car("JHF261G", "BMW", 2013, "Blue", 50000, true));
+        cars.add(new Car("HSFY23H", "Ford", 2012, "Black", 35000, false));
+        cars.add(new Car("GMDK353", "Volvo", 2014, "White", 40000, true));
+        cars.add(new Car("345GKM5", "Jaguar", 2011, "Gray", 48000, false));
+        cars.add(new Car("JETER36", "Volkswagen", 2012, "Black", 10000, true));
+        cars.add(new Car("3754HWH", "BMW", 2014, "Blue", 33000, true));
+        cars.add(new Car("YRTJD45", "Mercedes", 2011, "Red", 44000, false));
+        cars.add(new Car("FDGSH34", "Audi", 2010, "Yellow", 20000, false));
+        cars.add(new Car("GD534G", "Honda", 2012, "Black", 17000, false));
+    }
+ 
+    public String getFirstname() {
+        return firstname;
+    }
+ 
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+ 
+    public String getLastname() {
+        return lastname;
+    }
+ 
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+     
+    public void save() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("You've registered"));
+    }
+ 
+    public List<Car> getCars() {
+        return cars;
+    }
+ 
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+    
+}
---
+++ primefaces-test01/src/main/java/com/tomasmoreno/test/primefaces/beans/ResetInputView.java
@@ -0,0 +1,62 @@
+package com.tomasmoreno.test.primefaces.beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import org.primefaces.context.RequestContext;
+ 
+@ManagedBean
+public class ResetInputView {
+     
+    private String text1;
+     
+    private String text2;
+ 
+    public String getText1() {
+        
+    	return text1;
+    	
+    }
+ 
+    public void setText1( String text1 ) {
+        
+    	this.text1 = text1;
+    	
+    }
+ 
+    public String getText2() {
+        
+    	return text2;
+    	
+    }
+ 
+    public void setText2( String text2 ) {
+        
+    	this.text2 = text2;
+    	
+    }
+     
+    public void save() {
+        
+    	FacesContext.getCurrentInstance().addMessage( null, new FacesMessage( "Data Saved" ) );
+    	
+    }
+     
+    public void reset() {
+        
+    	RequestContext.getCurrentInstance().reset( "form:panel" );
+    	
+    }
+     
+    public void resetFail() {
+    
+    	this.text1 = null;
+        this.text2 = null;
+         
+        FacesMessage msg = new FacesMessage( "Model reset, but it won't work properly." );
+        
+        FacesContext.getCurrentInstance().addMessage( null, msg );
+    
+    }
+    
+}
